4d0cb8a26b973ac7dc75be2bed3a57182e954d49
mihovu <hoangvmh@gmail.com>
2019-06-22 13:01:48 +0900

    add lib

--- app/src/main/java/giavu/co/jp/spintplaning/MainActivity.kt
+++ app/src/main/java/giavu/co/jp/spintplaning/MainActivity.kt
@@ -1,14 +1,17 @@
 package giavu.co.jp.spintplaning
 
+import android.arch.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    val viewModel: MainViewModel by lazy {
+        ViewModelProvider
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
     }
 }
---
+++ app/src/main/java/giavu/co/jp/spintplaning/MainViewModel.kt
@@ -0,0 +1,11 @@
+package giavu.co.jp.spintplaning
+
+import android.arch.lifecycle.ViewModel
+
+/**
+ * @Author: Hoang Vu
+ * @Date:   2019-06-22
+ */
+class MainViewModel: ViewModel() {
+
+}
