1092c94b1b967198e880cd223baad763c6491b19
mihovu <hoangvmh@gmail.com>
2019-06-22 14:58:21 +0900

    Make app smoother

---
+++ app/src/main/java/giavu/co/jp/spintplaning/binding/BindingAdapter.kt
@@ -0,0 +1,13 @@
+package giavu.co.jp.spintplaning.binding
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+
+/**
+ * @Author: Hoang Vu
+ * @Date:   2019-06-22
+ */
+@BindingAdapter("onClick")
+fun setClick(view: View, listener: View.OnClickListener) {
+    view.setOnProtectBarrageClickListener(listener)
+}
