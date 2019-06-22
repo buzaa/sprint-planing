0f6b0c3cc6d86b7354d172f7cd63434237ce0284
mihovu <hoangvmh@gmail.com>
2019-06-22 15:14:34 +0900

    Temprory commit

--- app/src/main/java/giavu/co/jp/spintplaning/binding/BindingAdapter.kt
+++ app/src/main/java/giavu/co/jp/spintplaning/binding/BindingAdapter.kt
@@ -1,5 +1,6 @@
 package giavu.co.jp.spintplaning.binding
 
+import android.util.Log
 import android.view.View
 import androidx.databinding.BindingAdapter
 
@@ -7,7 +8,8 @@ import androidx.databinding.BindingAdapter
  * @Author: Hoang Vu
  * @Date:   2019-06-22
  */
-@BindingAdapter("onClick")
+@BindingAdapter("android:onClick")
 fun setClick(view: View, listener: View.OnClickListener) {
+    Log.d("BindingAdater", "Test")
     view.setOnProtectBarrageClickListener(listener)
 }
