134895410782af2429a5cbaaf8c496049ea1cf30
mihovu <hoangvmh@gmail.com>
2019-06-22 14:03:10 +0900

    Show number activity

--- app/src/main/java/giavu/co/jp/spintplaning/MainActivity.kt
+++ app/src/main/java/giavu/co/jp/spintplaning/MainActivity.kt
@@ -1,17 +1,42 @@
 package giavu.co.jp.spintplaning
 
-import android.arch.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import giavu.co.jp.spintplaning.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    val viewModel: MainViewModel by lazy {
-        ViewModelProvider
+    val viewModel by lazy {
+        ViewModelProviders.of(this).get(MainViewModel::class.java)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initialize()
+        observeViewModel()
+    }
+
+    private fun initialize() {
+        initializeDataBinding()
+    }
+
+    private fun initializeDataBinding() {
+        DataBindingUtil.setContentView<ActivityMainBinding>(
+            this, R.layout.activity_main
+        ).apply {
+            viewModel = this@MainActivity.viewModel
+            lifecycleOwner = this@MainActivity
+        }
+    }
+
+    private fun observeViewModel() {
+        with(viewModel) {
+            numberValue.observe(this@MainActivity, Observer {
+                startActivity(ShowNumberActivity.createIntent(this@MainActivity, it))
+            })
+        }
     }
 }
---
+++ app/src/main/java/giavu/co/jp/spintplaning/ShowNumberActivity.kt
@@ -0,0 +1,25 @@
+package giavu.co.jp.spintplaning
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ShowNumberActivity : AppCompatActivity() {
+
+    companion object {
+
+        const val KEY_NUMBER = "key_number"
+
+        fun createIntent(context: Context, number: String): Intent {
+            return Intent(context, ShowNumberActivity::class.java).apply {
+                putExtra(KEY_NUMBER, number)
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_number)
+    }
+}
