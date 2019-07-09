495e092977ea17c8e4ff8549cf0eee8be08d2e73
mihovu <hoangvmh@gmail.com>
2019-07-09 20:54:15 +0900

    Edit Information Activity

--- app/src/main/java/giavu/co/jp/spintplaning/information/InformationActivity.kt
+++ app/src/main/java/giavu/co/jp/spintplaning/information/InformationActivity.kt
@@ -3,19 +3,13 @@ package giavu.co.jp.spintplaning.information
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Base64
-import android.util.Log
 import android.view.MenuItem
-import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
-import de.psdev.licensesdialog.NoticesHtmlBuilder
-import de.psdev.licensesdialog.NoticesXmlParser
 import giavu.co.jp.spintplaning.R
 
 class InformationActivity : AppCompatActivity() {
 
     companion object {
-        private val TAG = InformationActivity::class.java.simpleName
         fun createIntent(context: Context): Intent {
             return Intent(context, InformationActivity::class.java)
         }
@@ -29,22 +23,6 @@ class InformationActivity : AppCompatActivity() {
 
     private fun initialize() {
         initializeActionBar()
-        setupWebView()
-    }
-
-    private fun setupWebView() {
-        val webView = findViewById<WebView>(R.id.web_view)
-        try {
-            val html = NoticesHtmlBuilder.create(applicationContext)
-                .setNotices(NoticesXmlParser.parse(resources.openRawResource(R.raw.notices)))
-                .build()
-            val data = Base64.encodeToString(html.toByteArray(), Base64.NO_PADDING)
-            webView.loadData(data, "text/html", "base64")
-        } catch (e: Exception) {
-            Log.e(TAG, e.toString())
-            finish()
-        }
-
     }
 
     private fun initializeActionBar() {
